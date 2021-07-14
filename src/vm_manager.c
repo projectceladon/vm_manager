38cd90f01b2dbe6e231252da3002400cf47e1012
Yadong Qi <yadong.qi@linux.intel.com>
2021-07-14 10:39:15 +0800

    Bump version to 0.4.1
    Bump version to 0.4.1
    
    Tracked-On: OAM-97758
    Signed-off-by: Yadong Qi <yadong.qi@linux.intel.com>

--- src/vm_manager.c
+++ src/vm_manager.c
@@ -20,8 +20,8 @@
 #include "vm_manager.h"
 
 #define VERSION_MAJOR 0
-#define VERSION_MINOR 3
-#define VERSION_MICRO 2
+#define VERSION_MINOR 4
+#define VERSION_MICRO 1
 
 #define VERSION xstr(VERSION_MAJOR)"."xstr(VERSION_MINOR)"."xstr(VERSION_MICRO)
 
