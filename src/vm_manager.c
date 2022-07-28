5a7a7083f5cfc97353804f33d595bff3669be7dc
Yadong Qi <yadong.qi@intel.com>
2022-07-28 14:12:37 +0800

    Bump version to 0.7.1
    Bump version to 0.7.1
    
    Tracked-On: OAM-103537
    Signed-off-by: Yadong Qi <yadong.qi@intel.com>

--- src/vm_manager.c
+++ src/vm_manager.c
@@ -20,7 +20,7 @@
 #include "vm_manager.h"
 
 #define VERSION_MAJOR 0
-#define VERSION_MINOR 6
+#define VERSION_MINOR 7
 #define VERSION_MICRO 1
 
 #define VERSION xstr(VERSION_MAJOR)"."xstr(VERSION_MINOR)"."xstr(VERSION_MICRO)
