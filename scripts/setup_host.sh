2b3717a501a8f79f2f69c46805019a4a796893ee
imititel <ionel-catalin.mititelu@intel.com>
2022-09-01 16:04:33 +0300

    Add openssl release version

--- scripts/setup_host.sh
+++ scripts/setup_host.sh
@@ -10,6 +10,7 @@ set -eE
 #---------      Global variable     -------------------
 reboot_required=0
 QEMU_REL="qemu-7.0.0"
+OPENSSL_REL="1.1.1q"
 CIV_WORK_DIR=$(pwd)
 CIV_GOP_DIR=$CIV_WORK_DIR/GOP_PKG
 CIV_VERTICAl_DIR=$CIV_WORK_DIR/vertical_patches/host
