69804c712e5a7a957cd52fa4aa762ed3b3e6a42d
Yadong Qi <yadong.qi@intel.com>
2022-09-16 09:38:48 +0800

    Install fixed version for S/stable
    Install fixed version for S/stable
    
    Tracked-On: OAM-103950
    Signed-off-by: Yadong Qi <yadong.qi@intel.com>

--- scripts/setup_host.sh
+++ scripts/setup_host.sh
@@ -218,15 +218,14 @@ function ubu_build_ovmf_gvt(){
 }
 
 function install_vm_manager_deb(){
-    #Try to download from latest release/tag
     local os_ver=$(lsb_release -rs)
-    local vm_repo="https://github.com/projectceladon/vm_manager/"
-    local rtag=$(git ls-remote -t --refs ${vm_repo} | cut --delimiter='/' --fields=3  | tr '-' '~' | sort --version-sort | tail --lines=1)
+    local vm_repo="https://github.com/projectceladon/vm_manager"
+    local rtag="v0.7.1"
     local rdeb=vm-manager_${rtag}_ubuntu-${os_ver}.deb
 
     [ -f ${rdeb} ] && rm -f ${rdeb}
 
-    local rurl=https://github.com/projectceladon/vm_manager/releases/latest/download/${rdeb}
+    local rurl=${vm_repo}/releases/download/${rtag}/${rdeb}
 
     if wget ${rurl} ; then
         sudo dpkg -i ${rdeb} || return -1
@@ -236,24 +235,13 @@ function install_vm_manager_deb(){
     fi
 }
 
-function install_vm_manager_src() {
-    #Try to build from source code
-    sudo apt-get install --yes make gcc
-    git clone https://github.com/projectceladon/vm_manager.git || return -1
-    cd vm_manager/
-    make || return -1
-    sudo make install || return -1
-    cd -
-    rm -rf vm_manager/
-}
-
 function install_vm_manager() {
     sudo apt-get update
     sudo apt-get install --yes libglib2.0-dev libncurses-dev libuuid1 uuid-dev libjson-c-dev wget lsb-release git
-    install_vm_manager_deb || install_vm_manager_src
+    install_vm_manager_deb
     if [ "$?" -ne 0 ]; then
         echo "Failed to install vm-manager!"
-        echo "Please download and install mannually from: https://github.com/projectceladon/vm_manager/releases/latest"
+        echo "Please download and install mannually from: https://github.com/projectceladon/vm_manager/releases/"
     fi
 }
 
