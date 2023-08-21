465b7332bcef0fa660eb9899541594c1b20a3547
Yadong Qi <yadong.qi@intel.com>
2023-08-21 10:01:31 +0800

    Update grpc to v1.57.0
    Remove patch to support vsock since it has been merged in grpc
    mainline since v1.57.0(https://github.com/grpc/grpc/commit/18c42a21af2331c4c755257a968490ab74c587b7).
    
    Signed-off-by: Yadong Qi <yadong.qi@intel.com>

--- src/cmake/gRPCFetchCMake.cmake
+++ src/cmake/gRPCFetchCMake.cmake
@@ -8,47 +8,17 @@
 
 include(FetchContent)
 
-macro(apply_git_patch REPO_PATH PATCH_PATH)
-  execute_process(COMMAND git apply --check ${PATCH_PATH}
-    WORKING_DIRECTORY ${REPO_PATH}
-    RESULT_VARIABLE SUCCESS
-    ERROR_QUIET)
-
-  if(${SUCCESS} EQUAL 0)
-    message("Applying git patch ${PATCH_PATH} in ${REPO_PATH} repository")
-    execute_process(COMMAND git am -3 ${PATCH_PATH}
-      WORKING_DIRECTORY ${REPO_PATH}
-      RESULT_VARIABLE SUCCESS)
-
-    if(${SUCCESS} EQUAL 1)
-      # We don't stop here because it can happen in case of parallel builds
-      message(WARNING "\nError: failed to apply the patch patch: ${PATCH_PATH}\n")
-    endif()
-  else()
-    message("Already applied: ${PATCH_PATH}")
-  endif()
-endmacro()
+set(ABSL_ENABLE_INSTALL ON)
 
 FetchContent_Declare(
   grpc
   GIT_REPOSITORY https://github.com/grpc/grpc
-  GIT_TAG        v1.52.1
+  GIT_TAG        v1.57.0
   GIT_SHALLOW 1
 )
 set(FETCHCONTENT_QUIET OFF)
 
-FetchContent_GetProperties(grpc)
-if (NOT grpc_POPULATED)
-  FetchContent_Populate(grpc)
-
-  #Apply VSOCK patches
-  file(GLOB patches_files "${CMAKE_SOURCE_DIR}/src/external/patches/grpc/*.patch")
-  foreach(file ${patches_files})
-    apply_git_patch(${grpc_SOURCE_DIR} ${file})
-  endforeach()
-
-  add_subdirectory(${grpc_SOURCE_DIR} ${grpc_BINARY_DIR} EXCLUDE_FROM_ALL)
-endif()
+FetchContent_MakeAvailable(grpc)
 
 set(_PROTOBUF_LIBPROTOBUF libprotobuf)
 set(_REFLECTION grpc++_reflection)
